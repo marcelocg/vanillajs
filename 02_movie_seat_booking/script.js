b165014ba1e4ca7f526f379f2bedd1e6107e6de5
Marcelo Goncalves <marcelo.goncalves@sefaz.ce.gov.br>
2020-02-27 16:59:10 -0300

    Complete interactivity implementation

--- 02_movie_seat_booking/script.js
+++ 02_movie_seat_booking/script.js
@@ -5,4 +5,22 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movie = document.getElementById('movie');
 
-const price = +movie.nodeValue;
+let price = +movie.value;
+
+function updateSelectedCount() {
+  const selectedSeatsCount = document.querySelectorAll('.row .seat.selected').length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * price;
+}
+
+container.addEventListener('click', (e) => {
+  if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+    e.target.classList.toggle('selected');
+    updateSelectedCount();
+  }
+});
+
+movie.addEventListener('change', (e) => {
+  price = +e.target.value;
+  updateSelectedCount();
+});
